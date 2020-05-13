53f2a1931b1cbccf353098b26f692b7cfa3a10de
root <root@DESKTOP-7OS8CPN.localdomain>
2020-05-13 15:16:03 +0530

    Yash Khanna 1527915

--- src/service/FlamesCheckService.java
+++ src/service/FlamesCheckService.java
@@ -1,4 +1,7 @@
+
 package service;
+
+import java.util.ArrayList;
 
 // Create a class called FlamesCheckService
 // FlamesCheckService has a method findFlames to find the flames between two names
@@ -7,3 +10,64 @@ package service;
 // You must return only the following values ['f','l','a','m','e','s']
 // change the return value at the end of the method corresponding to your return value
 
+public class FlamesCheckService{
+	
+	public char findFlames(String input1,String input2) {
+		char a='F';
+		ArrayList <String> s1=new ArrayList <String>();
+		ArrayList <String> s2=new ArrayList <String>();
+		for(int i=0;i<input1.length();i++) {
+			s1.add(input1.substring(i,i+1));
+		}
+		for(int i=0;i<input2.length();i++) {
+			s2.add(input2.substring(i,i+1));
+		}int k=0;
+		for(int i=0;i<s1.size();i++) {
+			System.out.print(s1.get(i)+" ");
+			if(s2.indexOf(s1.get(i))!=-1) {		
+				s2=flameLength11(s2,s1.get(i));
+				k++;
+			}
+		}
+		int flamesLength=s1.size()+s2.size()-k;
+		System.out.println(flamesLength);
+		ArrayList <String> flames1=new ArrayList<String>();
+		flames1.add("f");flames1.add("l");flames1.add("a");flames1.add("m");flames1.add("e");flames1.add("s");
+		int index=0;
+		while(flames1.size()!=1){
+			int col=0;
+			while(col<flamesLength-1){
+				System.out.print("\nFlames1 "+flames1+" Index: "+index+" "+" col "+col);
+				if(index>=flames1.size()-1){
+					index=index-flames1.size()+1;
+				}else{index++;}
+				col++;
+			}
+			System.out.print("\nI removed from "+index+" element "+flames1.get(index)+"\n");
+			flames1.remove(index);
+		}
+		//System.out.println(flames1);
+		//System.out.println(flamesLength);
+		//System.out.println(s1+"\n"+s2);
+		String s3=flames1.get(0);a=s3.charAt(0);
+		System.out.println("Your name is "+input1+"\nYour Partner name is "+input2+"\nyou two are ");
+		switch(a) {
+		case 'f':System.out.print("Friends.");break;
+		case 'l':System.out.print("Lovers.");break;
+		case 'a':System.out.print("Attracted to each other.");break;
+		case 'm':System.out.print("Married to each other");break;
+		case 'e':System.out.print("Enemy");break;
+		case 's':System.out.print("Sister");break;
+		}
+		//a='d';
+		return a;
+
+	}
+	
+	public static ArrayList<String> flameLength11(ArrayList<String> l1,String s2){
+		if(l1.indexOf(s2)!=-1){
+			l1.remove(s2);
+		}
+		return l1;
+	}
+}
--- src/testing/TestFlamesCheckService.java
+++ src/testing/TestFlamesCheckService.java
@@ -8,30 +8,30 @@ import service.FlamesCheckService;
 public class TestFlamesCheckService {
 	FlamesCheckService fcs = new FlamesCheckService();
 	@Test
-// 	public void testFindFlamesMethod() {
-// 		String name1 = "Alex";
-// 		String name2 = "Joylin";
-// 		char temp = 'a';
-// 		assertEquals(temp,fcs.findFlames(name1, name2));
-// 		name1 = "Steffe";
-// 		name2 = "Bobby";
-// 		temp = 'm';
-// 		assertEquals(temp,fcs.findFlames(name1, name2));
-// 		name1 = "John";
-// 		name2 = "Jully";
-// 		temp = 'e';
-// 		assertEquals(temp,fcs.findFlames(name1, name2));
-// 		name1 = "George";
-// 		name2 = "Neythiri";
-// 		temp = 'l';
-// 	    assertEquals(temp,fcs.findFlames(name1, name2));
-// 		try {
-// 			fcs.findFlames(null, null);
-// 			fcs.findFlames("AAA", null);
-// 			fcs.findFlames(null, "BBB");
-// 		}
-// 		catch(Exception e) {
-// 			e.printStackTrace();
-// 		}
-// 	}
+ 	public void testFindFlamesMethod() {
+ 		String name1 = "Alex";
+ 		String name2 = "Joylin";
+ 		char temp = 'a';
+ 		assertEquals(temp,fcs.findFlames(name1, name2));
+ 		name1 = "Steffe";
+ 		name2 = "Bobby";
+ 		temp = 'm';
+ 		assertEquals(temp,fcs.findFlames(name1, name2));
+ 		name1 = "John";
+ 		name2 = "Jully";
+ 		temp = 'e';
+ 		assertEquals(temp,fcs.findFlames(name1, name2));
+ 		name1 = "George";
+ 		name2 = "Neythiri";
+ 		temp = 'l';
+ 	    assertEquals(temp,fcs.findFlames(name1, name2));
+ 		try {
+ 			fcs.findFlames(null, null);
+ 			fcs.findFlames("AAA", null);
+ 			fcs.findFlames(null, "BBB");
+ 		}
+ 		catch(Exception e) {
+ 			e.printStackTrace();
+ 		}
+ 	}
 }
